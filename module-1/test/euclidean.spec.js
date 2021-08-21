f653fbae9615f89f7b0b1d8f0fcb29e25e6263b6
Sandor Simon <sandor_simon@epam.com>
2021-08-21 15:46:14 +0200

    jenkins

--- module-1/test/euclidean.spec.js
+++ module-1/test/euclidean.spec.js
@@ -16,7 +16,7 @@ describe('Module 1 - Euclidean algorithm', () => {
         [7, 9, 1],
         [6, 9, 3],
         [683, 997, 1]
-        
+
     ];
     values.forEach(v => {
         it(`should return ${v[2]} for (${v[0]},${v[1]})`, () => {
